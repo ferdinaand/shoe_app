5f49edd59eac42a9584c436fdbd0b77395c77011
ferdinaand <ekpo546@gmail.com>
2024-02-01 21:22:04 +0100

    update--shoeapp

---
+++ lib/resources/strings.res.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: constant_identifier_names
+String apiBaseUrl = 'https://riilfit-api.onrender.com';
+
+const countryCodeWithPlus = '+234';
+
+const countryCode = '234';
+
+///Error Messages
+const passwordTooWeak = 'password too weak';
+const passwordResetSuccessfulMessage =
+    'Password reset successfully, Kindly sign in to continue';
+const errorMessage =
+    "Sorry about this, Something's wrong but we are fixing it.";
+const networkError =
+    'No internet connection, Kindly check your internet to have a seamless experience.';
+
+///
+const maxJsonStringLength = 8192;
+
+const passwordRegEx =
+    r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$#!%*?&])[A-Za-z\d@$#!%*?&]{8,}$';
+
+const pinLength = 6;
