d096a380965bec27af42cdf10d6292fc9d0c202b
ferdinaand <ekpo546@gmail.com>
2024-02-01 21:21:57 +0100

    update--shoeapp

---
+++ lib/resources/radius.res.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+
+const xsmallRadius = Radius.circular(4);
+const smallRadius = Radius.circular(8);
+const regularRadius = Radius.circular(16);
+const mediumRadius = Radius.circular(24);
+const bigRadius = Radius.circular(30);
