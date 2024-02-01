e37e24a4f272848a124ca8ee56fc2fd752dc2f06
ferdinaand <ekpo546@gmail.com>
2024-02-01 21:21:42 +0100

    update--shoeapp

---
+++ lib/resources/colors.res.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/widgets.dart';
+
+///Primary color and it's variants
+const blue50 = Color(0xFFe7edf9);
+const blue100 = Color(0xFFb3c8ee);
+const blue200 = Color(0xFF8eade5);
+const blue300 = Color(0xFF5b87d9);
+const blue400 = Color(0xFF3b70d2);
+const blue500 = Color(0xFF0a4cc7);
+
+///neutral
+const grayScale50 = Color(0xFFf0f1f3);
+const grayScale100 = Color(0xFFcfd4db);
+const grayScale200 = Color(0xFFb8bfca);
+const grayScale300 = Color(0xFF97a2b1);
+const grayScale400 = Color(0xFF8390a2);
+const grayScale500 = Color(0xFF64748b);
+const grayScale600 = Color(0xFF5b6a7e);
+const grayScale700 = Color(0xFF475263);
+const grayScale800 = Color(0xFF37404c);
+const grayScale900 = Color(0xFF2a313a);
+
+///Alerts
+const info = Color(0xFF2F80ED);
+const success = Color(0xFF27AE60);
+const warning = Color(0xFFE2B93B);
+const error = Color(0xFFEB5757);
+
+///Others
+const white = Color(0xFFFFFFFF);
+const offerAccepted = Color(0xFFF0FDFA);
+const amber30 = Color(0xFFFEF3C7);
+const starColor = Color(0xFFFFCD3C);
+const amber600 = Color(0xFFD97706);
+const blueColor = Color.fromARGB(255, 29, 24, 189);
+const wineColor = Color(0xFFD1335E);
+const purple = Color.fromARGB(255, 117, 0, 219);
+const whatsappColor = Color(0xFF25d366);
+const twitterColor = Color(0xFF1da1f2);
+const instagramColor = Color(0xFFc13584);
+
+//green colors
+const success50 = Color(0xFFe9f9ef);
+const success100 = Color(0xFFbaedcd);
+const success200 = Color(0xFF99e4b5);
+const success300 = Color(0xFF6bd893);
+const success400 = Color(0xFF4ed17e);
+const success500 = Color(0xFF22c55e);
+const success600 = Color(0xFF1fb356);
+const success700 = Color(0xFF188c43);
+const success800 = Color(0xFF136c34);
+const success900 = Color(0xFF0e5327);
+
+//red colors
+const red50 = Color(0xFFfaeaea);
+const red100 = Color(0xFFefbdbd);
+const red200 = Color(0xFFe89d9d);
+const red300 = Color(0xFFdd7070);
+const red400 = Color(0xFFd65454);
+const red500 = Color(0xFFcc2929);
+const red600 = Color(0xFFba2525);
+const red700 = Color(0xFF911d1d);
+const red800 = Color(0xFF701717);
+const red900 = Color(0xFF561111);
