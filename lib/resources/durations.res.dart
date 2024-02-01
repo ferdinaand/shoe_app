41d88d9504497a4f25d947d2f293459faacc77f2
ferdinaand <ekpo546@gmail.com>
2024-02-01 21:21:45 +0100

    update--shoeapp

---
+++ lib/resources/durations.res.dart
@@ -0,0 +1,4 @@
+const superFastDuration = Duration(milliseconds: 50);
+const fastDuration = Duration(milliseconds: 200);
+const midDuration = Duration(milliseconds: 400);
+const slowDuration = Duration(milliseconds: 800);
