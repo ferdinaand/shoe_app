1d9ec6824342dae35315e4a5c5fd1bed7128dd34
ferdinaand <ekpo546@gmail.com>
2024-02-01 21:22:00 +0100

    update--shoeapp

---
+++ lib/resources/res.dart
@@ -0,0 +1,9 @@
+export 'colors.res.dart';
+export 'durations.res.dart';
+export 'icons.res.dart';
+export 'images.res.dart';
+export 'radius.res.dart';
+export 'strings.res.dart';
+export 'styles.res.dart';
+export 'typograhy.res.dart';
+export 'weights.res.dart';
