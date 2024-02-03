21ae391451c7ec8a8f5403e2e4b9dfd044f705f6
ferdinaand <ekpo546@gmail.com>
2024-02-03 23:11:42 +0100

    update--shoeapp

---
+++ lib/routing/app_routes.dart
@@ -0,0 +1,32 @@
+part of 'app_pages.dart';
+
+/// Contains all routes used in the app
+/// All routes must start with a backslash [\]
+abstract class Routes {
+  static const onboarding = '/app/onboarding';
+  static const home = '/app/home';
+  static const freightDetails = '/app/freightDetails';
+  static const dashLayout = '/app/dashLayout';
+  static const profileEdit = '/app/profileEdit';
+  static const truckManagerDashLayout = '/app/truckManagerDashLayout';
+  static const phoneScreen = '/app/phoneScreen';
+  static const verifyScreen = '/app/verifyScreen';
+  static const verifiedScreen = '/app/verifiedScreen';
+  static const createUser = '/app/createUser';
+  static const createTruckManager = '/app/createTruckManager';
+  static const getQuote = '/app/getQuote';
+  static const loadingScreen = '/app/loadingScreen';
+  static const availableDriversScreen = '/app/availableDriversScreen';
+  static const driverInfoScreen = '/app/driverInfoScreen';
+  static const activityDetailScreen = '/app/activityDetailScreen';
+  static const editProfile = '/app/editProfile';
+  static const editBankDetails = '/app/editBankDetails';
+  static const editTruckManagerProfile = '/app/editTruckManagerProfile';
+
+  static const registerTruck = '/app/registerTruck';
+  static const openWebView = '/app/openWebView';
+  static const gettingDriverScreen = '/app/gettingDriverScreen';
+  static const chooseUserType = '/app/chooseUserTyoe';
+  static const bankDetails = '/app/bankDetails';
+  static const assignDriver = '/app/assignDriver';
+}
