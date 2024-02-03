92fbf700719015cb4578f5ebb7b0dc2590cb3a16
ferdinaand <ekpo546@gmail.com>
2024-02-03 23:11:32 +0100

    update--shoeapp

---
+++ lib/resources/weights.res.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+const blackText = FontWeight.w900;
+const extraBoldText = FontWeight.w800;
+const boldText = FontWeight.w700;
+const semiBoldText = FontWeight.w600;
+const mediumText = FontWeight.w500;
+const regularText = FontWeight.w400;
+const lightText = FontWeight.w300;
+const ultralightText = FontWeight.w200;
