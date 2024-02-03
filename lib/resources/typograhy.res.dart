dfc5af6d61c8bbc3bd6874ff578f49bafadbe3ab
ferdinaand <ekpo546@gmail.com>
2024-02-03 23:11:20 +0100

    update--shoeapp

---
+++ lib/resources/typograhy.res.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/rendering.dart';
+
+import 'package:shoe_app/resources/weights.res.dart';
+
+const TextStyle heading1 = TextStyle(
+  fontSize: 56,
+  fontFamily: 'Gilroy',
+  fontWeight: semiBoldText,
+);
+
+const TextStyle heading2 = TextStyle(
+  fontSize: 48,
+  fontFamily: 'Gilroy',
+  fontWeight: semiBoldText,
+  height: 1.35,
+);
+
+const TextStyle heading3 = TextStyle(
+  fontSize: 40,
+  fontFamily: 'Gilroy',
+  fontWeight: semiBoldText,
+);
+
+const TextStyle heading4 = TextStyle(
+  fontSize: 32,
+  fontFamily: 'Gilroy',
+  fontWeight: boldText,
+);
+
+const TextStyle heading5 = TextStyle(
+  fontSize: 24,
+  fontFamily: 'Gilroy',
+  fontWeight: boldText,
+);
+
+const TextStyle heading6 = TextStyle(
+  fontSize: 20,
+  fontFamily: 'Gilroy',
+  fontWeight: boldText,
+);
+
+const TextStyle bodyXL = TextStyle(
+  fontFamily: 'Gilroy',
+  fontWeight: regularText,
+  fontSize: 18,
+);
+
+const TextStyle bodyL = TextStyle(
+  fontFamily: 'Gilroy',
+  fontSize: 20,
+  fontWeight: regularText,
+);
+
+const TextStyle bodyMed = TextStyle(
+  fontFamily: 'Gilroy',
+  fontSize: 18,
+  fontWeight: regularText,
+);
+const TextStyle bodyNorm = TextStyle(
+  fontFamily: 'Gilroy',
+  fontSize: 16,
+  fontWeight: regularText,
+);
+
+const TextStyle bodySmall = TextStyle(
+  fontFamily: 'Gilroy',
+  fontWeight: regularText,
+  fontSize: 14,
+);
+
+const TextStyle bodyXSmall = TextStyle(
+  fontFamily: 'Gilroy',
+  fontWeight: regularText,
+  fontSize: 10,
+);
+
+//bold fonts
+const TextStyle headingBold1 = TextStyle(
+  fontSize: 56,
+  fontFamily: 'GilroyBold',
+  fontWeight: semiBoldText,
+);
+
+const TextStyle headingBold2 = TextStyle(
+  fontSize: 48,
+  fontFamily: 'GilroyBold',
+  fontWeight: semiBoldText,
+  height: 1.35,
+);
+
+const TextStyle headingBold3 = TextStyle(
+  fontSize: 40,
+  fontFamily: 'GilroyBold',
+  fontWeight: semiBoldText,
+);
+
+const TextStyle headingBold4 = TextStyle(
+  fontSize: 32,
+  fontFamily: 'GilroyBold',
+  fontWeight: boldText,
+);
+
+const TextStyle headingBold5 = TextStyle(
+  fontSize: 24,
+  fontFamily: 'GilroyBold',
+  fontWeight: boldText,
+);
+
+const TextStyle headingBold6 = TextStyle(
+  fontSize: 20,
+  fontFamily: 'GilroyBold',
+  fontWeight: boldText,
+);
+
+const TextStyle bodyXLBold = TextStyle(
+  fontFamily: 'GilroyBold',
+  fontWeight: regularText,
+  fontSize: 18,
+);
+
+const TextStyle bodyLBold = TextStyle(
+  fontFamily: 'GilroyBold',
+  fontSize: 20,
+  fontWeight: regularText,
+);
+
+const TextStyle bodyMedBold = TextStyle(
+  fontFamily: 'GilroyBold',
+  fontSize: 18,
+  fontWeight: regularText,
+);
+const TextStyle bodyNormBold = TextStyle(
+  fontFamily: 'GilroyBold',
+  fontSize: 16,
+  fontWeight: regularText,
+);
+
+const TextStyle bodySmallBold = TextStyle(
+  fontFamily: 'GilroyBold',
+  fontWeight: regularText,
+  fontSize: 14,
+);
+
+const TextStyle bodyXSmallBold = TextStyle(
+  fontFamily: 'GilroyBold',
+  fontWeight: regularText,
+  fontSize: 10,
+);
+
+final hintTextStyle = bodyMed.copyWith(
+  height: 1.5,
+);
+
+final buttonTypography = bodyL.copyWith(
+  height: 1.5,
+  fontWeight: semiBoldText,
+);
